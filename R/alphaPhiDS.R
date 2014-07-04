e341719f489e0b9d6c59241f81b7955cce474d3f
agaye <ag13748@it017324.(none)>
2014-07-04 11:05:34 +0100

    Code amended.

--- R/alphaPhiDS.R
+++ R/alphaPhiDS.R
@@ -44,11 +44,11 @@ alphaPhiDS <- function (data, formula, family, clusterID, corstr, startBetas){
   mean.link <- f$link
   variance <- f$variance
   mean.link.v <- pmatch(mean.link, LINKS, -1, TRUE)
-  mu <- quasi(LINKS[mean.link.v])$linkinv(X.mat%*%startBetas)
+  mu <- as.numeric(quasi(LINKS[mean.link.v])$linkinv(X.mat%*%startBetas))
   
   # extracting summary statistics to calculate phi
-  pr2 <- (as.numeric(Y.vec) - as.numeric(mu))^2/f$variance(mu)
-  pr <-(as.numeric(Y.vec) - as.numeric(mu))/sqrt(f$variance(mu))
+  pr2 <- (Y.vec - mu)^2/f$variance(mu)
+  pr <-(Y.vec - mu)/sqrt(f$variance(mu))
   sum_p <- sum(pr2)
   # phi according to Zeger and Liang
   phi <- (N-npara)^{-1}*sum_p						 
