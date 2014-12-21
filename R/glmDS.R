d930b62ab4745cb9a0f4303d852ff7a8ee52fc66
agaye <ag13748@it017324.(none)>
2014-12-21 01:16:12 -0500

    'glmDS' amended and a new function 'checkNegValues' added, this function is required to check the 'weights' vector if specified.

--- R/glmDS.R
+++ R/glmDS.R
@@ -9,56 +9,58 @@
 #' @param beta.vect a string character: the starting values for the parameters in the linear predictor
 #' @param offset  null or a numreric vector that can be used to specify an a priori known component to be 
 #' included in the linear predictor during fitting.
+#' @param weights  an optional vector of 'prior weights' to be used in the fitting process. Should be NULL 
+#' or a numeric vector.
 #' @return a list which contains: the fitted \code{family}, a score vector and an information matrix
-#' @author Burton, P.; Laflamme, P.; Gaye, A.
+#' @author Burton, P.; Gaye, A.; Laflamme, P.
 #' @export
 #'
-glmDS <- function (formula, family, beta.vect=NULL, offset) {
-
-  mod.glm.ds <- glm(formula, family=family, x=TRUE, control=glm.control(maxit=1), constrast=NULL)
-
+glmdDS <- function (formula, family, beta.vect=NULL, offset, weights) {
+  
+  # reconstruct the formula that was re-written on the client side to protect some of the symbols
+  formula <- gsub( "TILDA", "~", formula, fixed=TRUE)
+  formula <- gsub("ASTERIX", "*", formula, fixed=TRUE) 
+  
+  # if the user did provide an offset vector alter the formula the include the offset vector
+  if(is.null(offset)){
+    formula <- as.formula(formula)    
+  }else{
+    offsetVector <- eval(parse(text=offset))
+    formulaText <- paste0(formula, "+offset(offsetVector)")
+    formula <- as.formula(formulaText)
+  }
+  
+  mod.glm.ds <- glm(formula, family=family, x=TRUE, control=glm.control(maxit=1), constrast=NULL, weights=eval(parse(text=weights)))
+  
   X.mat <- as.matrix(mod.glm.ds$x)
-
+  
   if(is.null(beta.vect)) {
     beta.vect <- rep(0,dim(X.mat)[2])
   }else{
     beta.vect <- as.numeric(unlist(strsplit(beta.vect, split=",")))
   }
-
+  
   numsubs<-dim(X.mat)[1]
-
+  
   y.vect<-as.vector(mod.glm.ds$y)
-
-  lp.vect.temp <- X.mat%*%beta.vect
-  if(is.null(offset)){
-    lp.vect <- lp.vect.temp
-  }else{
-    # check for missing values in the outcome vector 
-    # and delete the correpsonding entries in the offset vector
-    offsetVector <- eval(parse(text=offset))  
-    formulatext <- gsub(" ", "", deparse(formula), fixed=TRUE)
-    outcomeName <- strsplit(formulatext, split='~')[[1]][1]
-    outcomeVector <- eval(parse(text=outcomeName)) 
-    indxNA <- which(is.na(outcomeVector))
-    if(length(indxNA) > 0){offsetVector <- offsetVector[-indxNA]}
-    
-    lp.vect <- lp.vect.temp + offsetVector   
-  }
-
+  
+  lp.vect <- X.mat%*%beta.vect
+  
   f<-mod.glm.ds$family
-
+  
   mu.vect<-f$linkinv(lp.vect)
   mu.eta.val<-f$mu.eta(lp.vect)
   var.vect<-f$variance(mu.vect)
   dev<-sum(f$dev.resids(y.vect, mu.vect, rep(1, length(y.vect))))
-
+  
   W.vect<-as.vector(mu.eta.val^2/var.vect)
   WX.mat<-W.vect*X.mat
   info.matrix<-t(X.mat)%*%WX.mat
-
+  
   u.vect<-(y.vect-mu.vect)*1/var.vect
   W.u.mat<-matrix(W.vect*u.vect)
   score.vect<-t(X.mat)%*%W.u.mat
-
-  list(family=f, info.matrix=info.matrix, score.vect=score.vect, numsubs=numsubs, dev=dev)
+  
+  return(list(family=f, info.matrix=info.matrix, score.vect=score.vect, numsubs=numsubs, dev=dev))
+  
 }
