53f2ceb56282bee1188d303f3d02ae07e65ee839
agaye <ag13748@it017324.(none)>
2014-12-25 23:10:58 -0500

    'glmDS' amended to include 'offset' and 'weigts' parameters.

--- R/glmDS.R
+++ R/glmDS.R
@@ -3,61 +3,53 @@
 #' @description Fits a GLM for one iteration only
 #' @details This function cannot be called through 'datashield.aggregate' like other aggregate functions
 #' It is only called from within the client function \code{ds.glm} in the package \code{dsmodellingclient}
-#' @param formula an object of class \code{formula}
-#' @param family a description of the error distribution and link function to
-#' used in the model
-#' @param beta.vect a string character: the starting values for the parameters in the linear predictor
-#' @param offset  null or a numreric vector that can be used to specify an a priori known component to be 
-#' included in the linear predictor during fitting.
-#' @param weights  an optional vector of 'prior weights' to be used in the fitting process. Should be NULL 
-#' or a numeric vector.
+#' @param formula an object of class \code{formula}.
+#' @param family a character, description of the error distribution and link function to used in the model.
+#' @param beta.vect a string character: the starting values for the parameters in the linear predictor.
+#' @param offset  a character, null or a numeric vector that can be used to specify an a priori known component 
+#' to be included in the linear predictor during fitting.
+#' @param weights  a character, the name of an optional vector of 'prior weights' to be used in the fitting 
+#' process. Should be NULL or a numeric vector.
 #' @param data a character, the name of an optional data frame containing the variables in 
 #' in the \code{formula}. 
 #' @return a list which contains: the fitted \code{family}, a score vector and an information matrix
-#' @author Burton, P.; Gaye, A.; Laflamme, P.
+#' @author Gaye, A.; Burton, P.; Laflamme, P.
 #' @export
 #'
-glmDS <- function (formula, family, beta.vect=NULL, offset, weights, data) {
+glmDS <- function (formula, family, beta.vect, offset, weights, data) {
   
-  # reconstruct the formula that was re-written on the client side to protect some of the symbols
-  formula <- gsub( "TILDA", "~", formula, fixed=TRUE)
-  formula <- gsub("ASTERIX", "*", formula, fixed=TRUE) 
+  # get the value of the 'data' and 'weights' parameters provided as character on the client side
+  if(is.null(data)){
+    dataTable <- NULL 
+  }else{
+    dataTable <- eval(parse(text=data))
+  } 
   
-  # if the user did provide an offset vector alter the formula the include the offset vector
+  # this bit deals with cases where 'offset' is a loose vector or a vector within another data frame than 'data' or NULL
+  # reconstruct the formula to include an offset variable if the user did specify one
   if(is.null(offset)){
-    formula <- as.formula(formula)    
+    formula2use <- Reduce(paste, deparse(formula))
   }else{
     myterms <- unlist(strsplit(offset, split='$', fixed=TRUE))
     if(length(myterms) > 1){
-      offsetVector <- eval(parse(text=offset))
+      offsetname <- offset
     }else{
       if(!(is.null(data))){
-        offsetVector <- eval(parse(text=paste0(data, "$", offset)))       
+        offsetname <- paste0(data, "$", offset)      
       }else{
-        offsetVector <- eval(parse(text=offset))
+        offsetname <- offset
       }
     }
-    formulaText <- paste0(formula, "+offset(offsetVector)")
-    formula <- as.formula(formulaText)
+    formula2use <- as.formula(paste0(Reduce(paste, deparse(formula)), paste0(" + offset(", offsetname, ")")))    
   }
   
-  # get the value of the parameter provided as character in the client side
+  # if 'weights' are defined fit model accordingly
   if(is.null(weights)){
-    weightsVector <- eval(parse(text=weights))   
+    mod.glm.ds <- glm(formula2use, family=family, x=TRUE, control=glm.control(maxit=1), constrast=NULL, data=dataTable)
   }else{
-    myterms <- unlist(strsplit(weights, split='$', fixed=TRUE))
-    if(length(myterms) > 1){
-      weightsVector <- eval(parse(text=weights))
-    }else{
-      if(!(is.null(data))){
-        weightsVector <- eval(parse(text=paste0(data, "$", weights)))       
-      }else{
-        weightsVector <- eval(parse(text=weights))
-      }
-    }
+    mod2eval <- paste0("glm(",formula2use,  ",family=", family, ",x=TRUE,", "control=glm.control(maxit=1),constrast=NULL,data=dataTable,weights=",weights, ")")
+    mod.glm.ds <- eval(parse(text=mod2eval))
   }
-  dataTable <- eval(parse(text=data))
-  mod.glm.ds <- glm(formula, family=family, x=TRUE, control=glm.control(maxit=1), constrast=NULL, weights=weightsVector, data=dataTable)
   
   X.mat <- as.matrix(mod.glm.ds$x)
   
