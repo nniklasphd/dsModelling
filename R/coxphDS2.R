dc8c8bc83545c350073ff2957ff9e845f130ae62
nniklasphd <55879752+nniklasphd@users.noreply.github.com>
2019-09-30 12:23:01 +0200

    Update coxphDS2.R

--- R/coxphDS2.R
+++ R/coxphDS2.R
@@ -26,25 +26,48 @@ coxphDS2 <- function (data, survival_time, survival_event, terms, tuniq) {
   data_features <- dataset[, features]
   time_values   <- dataset[, survival_time]
   delta_values  <- dataset[, survival_event];
+ 
+  n <- length(tuniq)
+  m <- length(features)
+  s <- matrix(0,n,m)
+  d <- rep(0,n)
+  index <- rep(0,n)
+ 
+  for(i in 1:n)
+  {
+	if (length(time_values[time_values==tuniq[i]&delta_values==1])>1)
+		s[i,] <- colSums(data_features[time_values==tuniq[i]&delta_values==1,])
+	if (length(time_values[time_values==tuniq[i]&delta_values==1])==1)
+		s[i,] <- data_features[time_values==tuniq[i]&delta_values==1,]
+	if (length(time_values[time_values==tuniq[i]&delta_values==1])==0)
+		s[i,] <- rep(0,m)
+	d[i] <- length(time_values[time_values==tuniq[i]&delta_values==1])
+	index[i] <- length(time_values[time_values<tuniq[i]])+1
+  }
+  
+  
+ 
+ 
+ 
 
   # calculate index, Di, sumZ
-  no_t  <- length(tuniq)
-  index <- DI <- c()
-  sumZ  <- NULL
-  for (k in 1:no_t) {
-    T_idx      <- (time_values == tuniq[k])
-    index      <- c(index, sum(T_idx))
-    Delta_idx  <- (delta_values == 1)
-    total_idx  <- (T_idx & Delta_idx)
-    DI         <- c(DI, sum(total_idx))
-    col_sum    <- colSums(data_features[total_idx, ,drop = FALSE])
-    if (is.null(sumZ)) {
-      sumZ <- col_sum
-    } else {
-      sumZ <- rbind(sumZ, col_sum)
-    }
-  }
-  return(list(index = index, DI = DI, sum.Z = sumZ))
+  #no_t  <- length(tuniq)
+  #index <- DI <- c()
+  #sumZ  <- NULL
+  #for (k in 1:no_t) {
+  #  T_idx      <- (time_values == tuniq[k])
+  #  index      <- c(index, sum(T_idx))
+  #  Delta_idx  <- (delta_values == 1)
+  #  total_idx  <- (T_idx & Delta_idx)
+  #  DI         <- c(DI, sum(total_idx))
+  #  col_sum    <- colSums(data_features[total_idx, ,drop = FALSE])
+  #  if (is.null(sumZ)) {
+  #    sumZ <- col_sum
+  #  } else {
+  #    sumZ <- rbind(sumZ, col_sum)
+  #  }
+  #}
+  return(list(index = index, DI = d, sum.Z = s))
 }
 #coxphDS2
 
