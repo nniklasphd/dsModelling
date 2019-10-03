52e8af02c469c330e26ef25a8ea3246e97132b0f
nniklasphd <55879752+nniklasphd@users.noreply.github.com>
2019-10-03 11:12:45 +0200

    Update coxphDS3.R

--- R/coxphDS3.R
+++ R/coxphDS3.R
@@ -39,10 +39,7 @@ coxphDS3 <- function (data, survival_time, survival_event, terms, beta.vect, dat
 			}
 	}
 	
-  #for(i in 1:n){index[i]<- length(time_values[time_values==tuniq[i]&delta_values==1])+1}
-  #for(i in 1:n){index[i]<- length(time_values[time_values<tuniq[i]])+1}
-  #for(i in 1:length(data_times)){index[[i]] <-  which(!(time_values<data_times[i] & delta_values==0)&!(time_values<=data_times[i] & delta_values==1))}
-  
+   
   temp1 <- c(exp(data_features%*%beta.vect))
   ebz <- rep(0,length(data_times))
   zebz <- matrix(0,length(data_times),n_features)
@@ -60,28 +57,6 @@ coxphDS3 <- function (data, survival_time, survival_event, terms, beta.vect, dat
 	  zzebz[i,,] <- colSums((zz*temp1)[ind,,],dims=1)
           }
   }
-  #temp2 <- rev(cumsum(rev(temp1)))
-  #temp2 <- temp2[index]
-  #sum_matrix <- (apply(apply(apply(data_features*temp1,2,rev),2,cumsum),2,rev))[index,]
-  #sum_matrix <- apply(apply(apply(data_features*temp1,2,rev),2,cumsum),2,rev)/temp2[index,]
-    
-  #sum_array <- (apply(apply(apply(zz*temp1,c(2,3),rev),c(2,3),cumsum),c(2,3),rev))[index,,]
-  #sum_array <- (apply(apply(apply(zz*temp1,c(2,3),rev),c(2,3),cumsum),c(2,3),rev)/temp2)[index,,]
-
-  #tmp0 <- rep(0,length(data_times))
-  #tmp1 <- matrix(0,length(data_times),n_features)
-  #tmp2 <- array(0,c(length(data_times),n_features,n_features))
-  
-  #tmp0[is.element(data_times,tuniq)] <- temp2
-  #tmp1[is.element(data_times,tuniq),] <- sum_matrix
-  #tmp2[is.element(data_times,tuniq),,] <- sum_array
-  
-  #temp2 <- tmp0
-  #sum_matrix <- tmp1
-  #sum_array <- tmp2
-  
-  #sum_matrix <- matrix(0,length(data_times),n_features)
-  #sum_array <- array(0,c(length(data_times),n_features,n_features))
   
   return(list(ebz=ebz, zebz = zebz, zzebz = zzebz))
 }
