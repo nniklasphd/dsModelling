8df2452aab723b255d5825ee0aa5e2bb07db3d54
nniklasphd <55879752+nniklasphd@users.noreply.github.com>
2019-10-04 09:04:19 +0200

    Update coxphDS1.R

--- R/coxphDS1.R
+++ R/coxphDS1.R
@@ -5,10 +5,10 @@
 #' @param terms comma separated string containing the terms for the model
 #'
 #' @return a list of time values and aggregated statistics based on local data.
-#' @author Inberg, G.
+#' @author Niklas, N.
 #' @export
 #' 
-coxphDS1 <- function (data, survival_time, terms) {
+coxphDS1 <- function (data, survival_time) {
   # get the value of the 'data' parameter provided as character on the client side
   if(is.null(data)){
     dataset <- NULL 
@@ -16,15 +16,9 @@ coxphDS1 <- function (data, survival_time, terms) {
     dataset <- as.matrix(eval(parse(text=data)))
   }
   
-  #Convert parameters from transmittable (character) format to numeric 
-  features      <- unlist(strsplit(terms, split=","))
-  
   dataset       <- dataset[order(dataset[, survival_time]),]
-  data_features <- dataset[, features]
   time_values   <- dataset[, survival_time]
-  ZZvc          <- Conj(t.default(data_features)) %*% data_features
   
-  #return(list(ZZvc = ZZvc, time.values = time_values))
   return(list(time.values = time_values))
 }
 #coxphDS1
